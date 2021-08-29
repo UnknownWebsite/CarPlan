5c30e0ab8fda761d88a6743d6b2a9164c6e02c16
Gentleman-DE <jordibolz.jb@gmail.com>
2021-08-29 18:59:34 +0200

    navbar cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import './components/Kek/navbar.css'
 import './Global.css';
@@ -12,7 +11,6 @@ import {NavBar} from './components/Kek/navbar.js';
 
 
 
-
 function App() {
   
   const [darkMode, setDarkMode]  = useStickyState(true);
@@ -22,38 +20,17 @@ function App() {
   };
 
   return (
+    
     <ThemeProvider theme={darkMode === false ? lightTheme : darkTheme}>
-      <GlobalStyles />
+      <GlobalStyles /> 
+      
+      <NavBar />
+      
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <p>
-          <code>{NavBar}</code>
-          </p>
-
-          <div class="navbar">
-
-
-        <a href="#" class="navbar.active">Home</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#" class="navbar.right">Link</a>
-          <a>Hello Wardu</a>
-        </div>
-  
-          
-        </a>
+     
+        
         <ToogleButton onChangeToActive={() => setDarkMode(true)} onChangeToInactive={() => setDarkMode(false)}></ToogleButton>
       </header>
     </div>
--- src/components/Kek/navbar.js
+++ src/components/Kek/navbar.js
@@ -1,17 +1,18 @@
 export function NavBar(){
   
-<head><link rel="stylesheet" href="navbar.css"></link>
+<head>
+  <link rel="stylesheet" href="navbar.css"></link>
 </head>
 
 return(
 <div class="navbar">
 
 
-      <a href="#" class="navbar.active">Home</a>
+      <a href="" class="navbar.active">Home</a>
       <a href="#">Link</a>
       <a href="#">Link</a>
-      <a href="#" class="navbar.right">Link</a>
-      <a>Hello Wardu</a>
+      <a href="#" class="navbar.right">Hello Wardu</a>
+      
     </div>
 )
     }
